e0f2185f0b179fba3ea75d2bd4d33f4323e3c8ae
iceyisaak <me@iceyisaak.com>
2026-01-28 17:25:33 +0100

    Bug fixed

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,15 @@
 from collections.abc import AsyncGenerator
+from sqlalchemy import TypeDecorator, CHAR, event
 import uuid
-
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 from datetime import datetime
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+from fastapi import Depends
+
+
 
 
 DATABASE_URL="sqlite+aiosqlite:///./test.db"
@@ -15,19 +19,54 @@ class Base(DeclarativeBase):
     pass
 
 
+class User(SQLAlchemyBaseUserTableUUID,Base):
+    posts = relationship("Post", back_populates="user", cascade="all, delete-orphan")
+
+
+class GUID(TypeDecorator):
+    impl = CHAR
+    cache_ok = True
+
+    def load_dialect_impl(self, dialect):
+        return dialect.type_descriptor(CHAR(36))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        return str(value) # This ensures the dashes are kept
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        return uuid.UUID(value)
+
+
 class Post(Base):
     __tablename__="posts"
 
-    id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    # id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    # user_id = Column(UUID(as_uuid=True),ForeignKey("user.id"),nullable=False)
+    # user_id = Column(UUID(as_uuid=True), ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    id = Column(GUID(), primary_key=True, default=uuid.uuid4)
+    user_id = Column(GUID(), ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     caption = Column(Text)
     url = Column(String,nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
+    user = relationship("User",back_populates="posts")
+
+
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+@event.listens_for(engine.sync_engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 
 async def create_db_and_tables():
@@ -38,3 +77,7 @@ async def create_db_and_tables():
 async def get_async_session()-> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+async def get_user_db(session:AsyncSession=Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
